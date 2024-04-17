7dd3d3f8a534e604ded998aaf077d66d45fe7174
Thy Nguyen Le Bao <nglbaothy212@gmail.com>
2024-04-17 17:22:03 +0700

    workshop

--- selenium/homework/register.spec.js
+++ selenium/homework/register.spec.js
@@ -70,6 +70,62 @@ describe('Register page', function () {
   
     assert.equal(errorMessage, 'Username already exists');
   });
+
+  //Hong Di chua lam validation :v
+  // it('Register with empty email and password field', async function () {
+  //   await driver.get('https://hoangduy0610.github.io/ncc-sg-automation-workshop-1/register.html');
+  
+  //   // moi khi findElement doi 500ms ~ 0.5s
+  //   await driver.manage().setTimeouts({ implicit: 500 });
+  
+  //   let submitButton = await driver.findElement(By.css('button'));
+
+  //   await submitButton.click();
+  //   await driver.manage().setTimeouts({ implicit: 1000 });
+
+  //   let message = await driver.findElement(By.className('alert-danger'));
+  //   let errorMessage = await message.getText();
+  
+  //   assert.equal(errorMessage, 'Registration failed');
+  // });
+
+  // it('Register with empty email field', async function () {
+  //   await driver.get('https://hoangduy0610.github.io/ncc-sg-automation-workshop-1/register.html');
+  
+  //   // moi khi findElement doi 500ms ~ 0.5s
+  //   await driver.manage().setTimeouts({ implicit: 500 });
+  
+  //   let password = await driver.findElement(By.id('password'));
+  //   let submitButton = await driver.findElement(By.css('button'));
+  
+  //   await password.sendKeys('Thy123');
+  //   await submitButton.click();
+  //   await driver.manage().setTimeouts({ implicit: 1000 });
+
+  //   let message = await driver.findElement(By.className('alert-danger'));
+  //   let errorMessage = await message.getText();
+  
+  //   assert.equal(errorMessage, 'Registration failed');
+  // });
+
+  // it('Register with empty password field', async function () {
+  //   await driver.get('https://hoangduy0610.github.io/ncc-sg-automation-workshop-1/register.html');
+  
+  //   // moi khi findElement doi 500ms ~ 0.5s
+  //   await driver.manage().setTimeouts({ implicit: 500 });
+  
+  //   let userName = await driver.findElement(By.id('email'));
+  //   let submitButton = await driver.findElement(By.css('button'));
+  
+  //   await userName.sendKeys('Thytest03');
+  //   await submitButton.click();
+  //   await driver.manage().setTimeouts({ implicit: 1000 });
+
+  //   let message = await driver.findElement(By.className('alert-danger'));
+  //   let errorMessage = await message.getText();
+  
+  //   assert.equal(errorMessage, 'Registration failed');
+  // });
   
   after(async () => await driver.quit());
 });
